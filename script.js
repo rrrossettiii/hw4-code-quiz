0c47adec42625117f15407f20113aa83b6899579
rrrossettiii <rrrossettiii@gmail.com>
2020-10-06 16:34:55 -0400

    Added 4 Questions

---
+++ script.js
@@ -0,0 +1,63 @@
+// timer
+// score keeper
+// high scores
+// start button
+
+// Questions
+// 
+// Q = The Question; A = The Answers; C = The Correct Choice;
+var allQuestions = {
+    q1 = {
+        Q: 'What was the first 1080p video on YouTube?',
+        A: ['A.) "Numa Numa"', 'B.) The Muppets (singing) - "Bohemian Rhapsody"', 'C.) Rick Astley - "Never Gonna Give You Up"', 'D.) "Chocolate Rain"'],
+        C: 1
+    },
+    q2 = {
+        Q: '10-20% of power outages are caused by what animal?',
+        A: ["A.) Kite Flying", "B.) Pigeons", "C.) Squirrels", "D.) Toaster Ovens"],
+        C: 2
+    },
+    q3 = {
+        Q: 'Which of these is the "Powerhouse" of the cell?',
+        A: ["A.) ATP", "B.) Mitochondria", "C.) Carbohydrates", "D.) Nucleus"],
+        C: 0
+    },
+    q4 = {
+        Q: 'What is the national animal of Scotland?',
+        A: ["A.) The Highland Cow", "B.) The Loch Ness Monster", "C.) The Bottle-nosed Dolphin", "D.) The Unicorn"],
+        C: 3
+    },
+    q5 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+    q6 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+    q7 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+    q8 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+    q9 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+    q10 = {
+        Q: 'what?',
+        A: ["A.)", "B.)", "C.)", "D.)"],
+        C: 0
+    },
+}
+
+// button list
+// correct/wrong

9d0d6037f2f11260ac55bd3ea72c5687cd6e769f
rrrossettiii <rrrossettiii@gmail.com>
2020-10-08 15:49:45 -0400

    added function that determines correct answer

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -42,19 +42,19 @@ var allQuestions = [
 			'Rick Astley - "Never Gonna Give You Up"',
 			'"Chocolate Rain"',
 		],
-		C: 1,
+		C: 'The Muppets (singing) - "Bohemian Rhapsody"',
 		E: "Is this the real life? Is this just fantasy?",
 	},
 	{
 		Q: "10-20% of U.S. power outages are caused by which of these factors?",
 		A: ["Kite Flying", "Pigeons", "Squirrels", "Toaster Ovens"],
-		C: 2,
-		E: "Squirrels never stop teething.",
+		C: "Squirrels",
+		E: "Squirrels never stop teething their whole lives.",
 	},
 	{
 		Q: 'Which of these is the "Powerhouse" of the cell?',
 		A: ["ATP", "Mitochondria", "Carbohydrates", "The Nucleus"],
-		C: 0,
+		C: "Mitochondria",
 		E: "MITOCHONDRIA IS THE POWERHOUSE OF THE CELL!",
 	},
 	{
@@ -65,27 +65,27 @@ var allQuestions = [
 			"The Bottle-nosed Dolphin",
 			"The Unicorn",
 		],
-		C: 3,
+		C: "The Unicorn",
 		E:
 			"In Celtic mythology, The Unicorn is a symbol of purity and innocence, as well as masculinity and power.",
 	},
 	{
 		Q: "The Washington Monument is capped with which metal?",
 		A: ["Aluminum", "Bronze", "Steel", "Tin"],
-		C: 0,
+		C: "Aluminum",
 		E:
 			"At the time it was built, in 1848, Aluminum was the most valuable metal.",
 	},
 	{
 		Q: "Which of these planets rotates clockwise?",
 		A: ["Earth", "Venus", "Jupiter", "Mars"],
-		C: 1,
+		C: "Venus",
 		E: "Venus is the only planet in our solar system that rotates clockwise.",
 	},
 	{
 		Q: "What are the caps on the end of shoelaces called?",
 		A: ["Aglet", "Grommet", "Eyelet", "Rivet"],
-		C: 0,
+		C: "Aglet",
 		E: "Aglets keep the fibers of the lace or cord from unraveling.",
 	},
 	{
@@ -96,21 +96,21 @@ var allQuestions = [
 			"one trillionth of a second.",
 			"30 Seconds.",
 		],
-		C: 1,
+		C: "one trillionth of a second.",
 		E:
 			'A "Jiffy" is the amount of time it takes light to travel one centimeter in a vacuum.',
 	},
 	{
 		Q: "Who invented the word 'nerd?'",
 		A: ["Stephen Hawking", "Earnest Hemingway", "Julius Caesar", "Dr. Seuss"],
-		C: 0,
+		C: "Stephen Hawking",
 		E: "You Nerd!",
 	},
 	{
 		Q:
 			"If you open your eyes in a pitch-black room, the color you'll see is called ____ ?",
 		A: ["Vantablack", "Taupe", "Eigengrau", "Davy's gray"],
-		C: 0,
+		C: "Eigengrau",
 		E:
 			'Eigengrau is a german word meaning "own grey" and is the intrinsic color we see in the absence of all light.',
 	},
@@ -138,42 +138,52 @@ function renderQuestions() {
 }
 renderQuestions();
 
-// Array Shuffler
-//
-function arrayShuffler(arrayToShuffle) {
-	// Variables - new array; how many shuffles; random index default;
-	shuffledArray = [];
-	i = arrayToShuffle.length;
-	j = 0;
+function shuffle(array) {
+	// Variables - how many remaining shuffles; output to back of array; random index;
+	var m = array.length,
+		t,
+		i;
 
-	// Loop - generate random index; adds this index to new array; deletes this index from 'arrayToShuffle';
-	while (i--) {
-		j = Math.floor(Math.random() * (i + 1));
-		shuffledArray.push(arrayToShuffle[j]);
-		arrayToShuffle.splice(j, 1);
+	// While there remain elements to shuffle…
+	while (m) {
+		// Pick a remaining element…
+		i = Math.floor(Math.random() * m--);
+
+		// And swap it with the current element.
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
 	}
-	console.log(shuffledArray);
+
+	return array;
 }
 
-arrayShuffler(allQuestions[0].A);
+let elementsArray = document.querySelectorAll("#answerBtn");
 
-// function shuffle(array) {
-//     var i = array.length,
-//         j = 0,
-//         temp;
+elementsArray.forEach(function (elem) {
+	elem.addEventListener("click", findAnswer);
+});
 
-//     while (i--) {
+function findAnswer(event) {
+	console.log(event.currentTarget.textContent);
+	console.log(allQuestions[0].C);
+	if (event.currentTarget.textContent.includes(allQuestions[0].C)) {
+		displayCorrect();
+	} else {
+		displayIncorrect();
+	}
+}
 
-//         j = Math.floor(Math.random() * (i+1));
+function displayCorrect() {
+	// select #result; change #result; display #result;
+	var result = document.querySelector("#result");
+	result.textContent = "Correct";
+	result.style.visibility = "visible";
+}
 
-//         // swap randomly chosen element with current element
-//         temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-
-//     }
-
-//     return array;
-// }
-
-// var shuffledArray = shuffle([1,2,3,4,5,6,7,8,9,10]);
+function displayIncorrect() {
+	// select #result; change #result; display #result;
+	var result = document.querySelector("#result");
+	result.textContent = "Wrong";
+	result.style.visibility = "visible";
+}
